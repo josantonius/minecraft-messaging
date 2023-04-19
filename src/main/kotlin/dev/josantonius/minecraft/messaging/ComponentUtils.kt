172b9b7a98ab38c9c2df2f51e05a7b02b7a474f8
Josantonius <hello@josantonius.dev>
2023-04-19 03:38:11 +0200

    feat: add option to pass custom hover message

--- src/main/kotlin/dev/josantonius/minecraft/messaging/ComponentUtils.kt
+++ src/main/kotlin/dev/josantonius/minecraft/messaging/ComponentUtils.kt
@@ -6,11 +6,23 @@ import net.kyori.adventure.text.event.ClickEvent
 import net.kyori.adventure.text.event.HoverEvent
 
 object ComponentUtils {
-    private val linkPattern =
-            Pattern.compile("((?:§[0-9a-fA-Fk-oK-OrR])+)<link(?:=(.+?))*>(.+?)</link>")
-    private val commandPattern =
-            Pattern.compile("((?:§[0-9a-fA-Fk-oK-OrR])+)<command(?:=(.+?))*>(.+?)</command>")
 
+    /**
+     * Creates a Component based on the input string. Supports the following input formats for
+     * creating clickable components:
+     * - <link>http://example.com</link>
+     * - <link=http://example.com>Visit our website.</link>
+     * - <command>/example</command>
+     * - <command=/example>Click here</command>
+     *
+     * For input strings without any of these formats, the function returns a non-clickable text
+     * component.
+     *
+     * @param input The input string containing the link or command.
+     * @param hoverMessages A map with custom hover messages for link and command components.
+     *
+     * @return A Component that may be clickable, depending on the input string.
+     */
     fun parseClickableComponents(
             input: String,
             hoverMessages: Map<String, String> = mapOf(),
@@ -18,72 +30,75 @@ object ComponentUtils {
         if (!input.contains("<link") && !input.contains("<command")) {
             return Component.text(input)
         }
-
+        val linkPattern =
+                Pattern.compile("((?:§[0-9a-fA-Fk-oK-OrR])+)<link(?:=(.+?))*>(.+?)</link>")
+        val commandPattern =
+                Pattern.compile("((?:§[0-9a-fA-Fk-oK-OrR])+)<command(?:=(.+?))*>(.+?)</command>")
         val components = mutableListOf<Component>()
+
         var lastIndex = 0
-
         while (true) {
-            val (nextIndex, component) = findNextClickableComponent(input, lastIndex, hoverMessages)
-            if (nextIndex == -1) break
+            val linkMatcher = linkPattern.matcher(input)
+            val commandMatcher = commandPattern.matcher(input)
 
-            val prefix = input.substring(lastIndex, nextIndex)
-            if (prefix.isNotEmpty()) components.add(Component.text(prefix))
+            val linkFound = linkMatcher.find(lastIndex)
+            val commandFound = commandMatcher.find(lastIndex)
 
-            components.add(component)
-            lastIndex = nextIndex
+            var nextIndex: Int
+            if (linkFound && (!commandFound || linkMatcher.start() < commandMatcher.start())) {
+                nextIndex = linkMatcher.start()
+
+                val prefix = input.substring(lastIndex, nextIndex)
+                if (prefix.isNotEmpty()) {
+                    components.add(Component.text(prefix))
+                }
+
+                val colorCode = linkMatcher.group(1)
+                val url = linkMatcher.group(2) ?: linkMatcher.group(3)
+                val text = "$colorCode${linkMatcher.group(3)}"
+                val linkHoverText = (hoverMessages["link"] ?: "Click to open") + " $url"
+                val linkComponent = Component.text(text)
+                components.add(
+                        linkComponent
+                                .clickEvent(ClickEvent.openUrl(url))
+                                .hoverEvent(HoverEvent.showText(Component.text(linkHoverText)))
+                )
+                lastIndex = linkMatcher.end()
+            } else if (commandFound && (!linkFound || commandMatcher.start() < linkMatcher.start())
+            ) {
+                nextIndex = commandMatcher.start()
+
+                val prefix = input.substring(lastIndex, nextIndex)
+                if (prefix.isNotEmpty()) {
+                    components.add(Component.text(prefix))
+                }
+
+                val colorCode = commandMatcher.group(1)
+                val command = commandMatcher.group(2) ?: commandMatcher.group(3)
+                val text = "$colorCode${commandMatcher.group(3)}"
+                val commandHoverText = (hoverMessages["command"] ?: "Click to run") + " $command"
+                val commandComponent = Component.text(text)
+                components.add(
+                        commandComponent
+                                .clickEvent(ClickEvent.runCommand(command))
+                                .hoverEvent(HoverEvent.showText(Component.text(commandHoverText)))
+                )
+                lastIndex = commandMatcher.end()
+            } else {
+                break
+            }
         }
 
         if (lastIndex < input.length) {
             val suffix = input.substring(lastIndex)
             components.add(Component.text(suffix))
         }
-
-        return components.joinToComponent()
-    }
-
-    private fun findNextClickableComponent(
-            input: String,
-            lastIndex: Int,
-            hoverMessages: Map<String, String>
-    ): Pair<Int, Component> {
-        val linkMatcher = linkPattern.matcher(input)
-        val commandMatcher = commandPattern.matcher(input)
-
-        val linkFound = linkMatcher.find(lastIndex)
-        val commandFound = commandMatcher.find(lastIndex)
-
-        return when {
-            linkFound && (!commandFound || linkMatcher.start() < commandMatcher.start()) -> {
-                val colorCode = linkMatcher.group(1)
-                val url = linkMatcher.group(2) ?: linkMatcher.group(3)
-                val text = "$colorCode${linkMatcher.group(3)}"
-                val linkHoverText = (hoverMessages["link"] ?: "Click to open") + " $url"
-                val linkComponent =
-                        Component.text(text)
-                                .clickEvent(ClickEvent.openUrl(url))
-                                .hoverEvent(HoverEvent.showText(Component.text(linkHoverText)))
-                linkMatcher.end() to linkComponent
-            }
-            commandFound && (!linkFound || commandMatcher.start() < linkMatcher.start()) -> {
-                val colorCode = commandMatcher.group(1)
-                val command = commandMatcher.group(2) ?: commandMatcher.group(3)
-                val text = "$colorCode${commandMatcher.group(3)}"
-                val commandHoverText = (hoverMessages["command"] ?: "Click to run") + " $command"
-                val commandComponent =
-                        Component.text(text)
-                                .clickEvent(ClickEvent.runCommand(command))
-                                .hoverEvent(HoverEvent.showText(Component.text(commandHoverText)))
-                commandMatcher.end() to commandComponent
-            }
-            else -> -1 to Component.empty()
-        }
-    }
-
-    private fun List<Component>.joinToComponent(): Component {
+        val updatedComponents = components.map { it.asComponent() }
         var result = Component.empty()
-        for (component in this) {
+        for (component in updatedComponents) {
             result = result.append(component)
         }
+
         return result
     }
 }
