cc6c0f35472d45753a695999a63a3fe412421b7a
Josantonius <hello@josantonius.dev>
2023-05-01 20:33:11 +0200

    feat: upgrade to v1.1.0 version

--- src/main/kotlin/dev/josantonius/minecraft/messaging/Message.kt
+++ src/main/kotlin/dev/josantonius/minecraft/messaging/Message.kt
@@ -1,8 +1,14 @@
 package dev.josantonius.minecraft.messaging
 
 import java.io.File
+import java.io.FileReader
+import java.io.IOException
 import net.kyori.adventure.platform.bukkit.BukkitAudiences
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.Bukkit
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -14,15 +20,25 @@ import org.bukkit.plugin.java.JavaPlugin
  *
  * @throws IllegalArgumentException if there is an error loading the file.
  */
-class Message(private val file: File, private val plugin: JavaPlugin) : MessageBase(file) {
+class Message(private val file: File, private val plugin: JavaPlugin) {
+    private val miniMessage = MiniMessage.miniMessage()
+    private val messages: FileConfiguration
+    private var consolePrefix: String = ""
+    private var chatPrefix: String = ""
+
     /**
-     * Sends a message with the given key and optional parameters to all online players.
+     * Loads the message file and stores the messages in the FileConfiguration.
      *
-     * @param key The key associated with the message in the message file.
-     * @param params Optional parameters to replace placeholders in the message.
+     * @throws IllegalArgumentException if there is an error loading the file.
      */
-    fun sendToAll(key: String, vararg params: String) {
-        Bukkit.getServer().sendMessage(getComponent(key, *params))
+    init {
+        try {
+            FileReader(file).use { reader ->
+                messages = YamlConfiguration.loadConfiguration(reader)
+            }
+        } catch (e: IOException) {
+            throw IllegalArgumentException("Error loading message file", e)
+        }
     }
 
     /**
@@ -33,18 +49,87 @@ class Message(private val file: File, private val plugin: JavaPlugin) : MessageB
      * @param params Optional parameters to replace placeholders in the message.
      */
     fun sendToPlayer(player: Player, key: String, vararg params: String) {
-        player.sendMessage(getComponent(key, *params))
+        var message = chatPrefix + getString(key, *params)
+        player.sendMessage(miniMessage.deserialize(message))
     }
 
     /**
-     * Sends a system message with a level to the console.
+     * Sends a message with the given key and optional parameters to all online players.
+     *
+     * @param key The key associated with the message in the message file.
+     * @param params Optional parameters to replace placeholders in the message.
+     */
+    fun sendToPlayers(key: String, vararg params: String) {
+        Bukkit.getServer().getOnlinePlayers().forEach { player ->
+            sendToPlayer(player, key, *params)
+        }
+    }
+
+    /**
+     * Sends a system message to the console.
      *
      * @param key The key associated with the message in the message file.
      * @param params Optional parameters to replace placeholders in the message.
      */
     fun sendToConsole(key: String, vararg params: String) {
-        val component = getComponent(key, *params)
+        var message = consolePrefix + getString(key, *params)
         val adventure = BukkitAudiences.create(plugin)
-        adventure.console().sendMessage(component)
+        adventure.console().sendMessage(miniMessage.deserialize(message))
+    }
+
+    /**
+     * Sends a message with the given key and optional parameters to online players and the console.
+     *
+     * @param key The key associated with the message in the message file.
+     * @param params Optional parameters to replace placeholders in the message.
+     */
+    fun broadcast(key: String, vararg params: String) {
+        sendToConsole(key, *params)
+        sendToPlayers(key, *params)
+    }
+
+    /**
+     * Set the prefix for console messages.
+     *
+     * @param prefix The prefix to use for console messages.
+     */
+    fun setConsolePrefix(prefix: String) {
+        consolePrefix = prefix
+    }
+
+    /**
+     * Set the prefix for chat messages.
+     *
+     * @param prefix The prefix to use for chat messages.
+     */
+    fun setChatPrefix(prefix: String) {
+        chatPrefix = prefix
+    }
+
+    /**
+     * Retrieves associated with the given key, replaces placeholders and returns a string.
+     *
+     * @param key The key associated with the message in the message file.
+     * @param params Optional parameters to replace placeholders in the message.
+     */
+    fun getString(key: String, vararg params: String): String {
+        var message = messages.getString(key)
+        if (message != null) {
+            for (i in params.indices) {
+                message = message?.replace("{" + (i + 1) + "}", params[i])
+            }
+        }
+        return message ?: key
+    }
+
+    /**
+     * Retrieves associated with the given key, replaces placeholders and returns a Component.
+     *
+     * @param key The key associated with the message in the message file.
+     * @param params Optional parameters to replace placeholders in the message.
+     */
+    fun getComponent(key: String, vararg params: String): Component {
+        var message = getString(key, *params)
+        return miniMessage.deserialize(message)
     }
 }
--- src/main/kotlin/dev/josantonius/minecraft/messaging/Title.kt
+++ src/main/kotlin/dev/josantonius/minecraft/messaging/Title.kt
@@ -1,9 +1,14 @@
 package dev.josantonius.minecraft.messaging
 
 import java.io.File
+import java.io.FileReader
+import java.io.IOException
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.title.Title as AdventureTitle
 import org.bukkit.Bukkit
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
 
 /**
@@ -14,24 +19,20 @@ import org.bukkit.entity.Player
  *
  * @throws IllegalArgumentException if there is an error loading the file.
  */
-class Title(private val file: File, private val times: AdventureTitle.Times? = null) :
-        MessageBase(file) {
+class Title(private val file: File, private val times: AdventureTitle.Times? = null) {
+    private val miniMessage = MiniMessage.miniMessage()
+    private val titles: FileConfiguration
 
     /**
-     * Shows a title and subtitle with the given keys and optional parameters to all online players.
+     * Loads the message file and stores the title in the FileConfiguration.
      *
-     * @param titleKey The key associated with the title message in the message file.
-     * @param subtitleKey The key associated with the subtitle message in the message file, or an
-     * empty string for no subtitle.
-     * @param params Optional parameters to replace placeholders in the messages.
+     * @throws IllegalArgumentException if there is an error loading the file.
      */
-    fun showToAll(titleKey: String, subtitleKey: String, vararg params: String) {
-        val titleComponent = getComponent(titleKey, *params)
-        val subtitleComponent =
-                if (subtitleKey.isNotEmpty()) getComponent(subtitleKey, *params)
-                else Component.empty()
-        for (player in Bukkit.getOnlinePlayers()) {
-            player.showTitle(AdventureTitle.title(titleComponent, subtitleComponent, times))
+    init {
+        try {
+            FileReader(file).use { reader -> titles = YamlConfiguration.loadConfiguration(reader) }
+        } catch (e: IOException) {
+            throw IllegalArgumentException("Error loading message file", e)
         }
     }
 
@@ -50,5 +51,50 @@ class Title(private val file: File, private val times: AdventureTitle.Times? = n
                 if (subtitleKey.isNotEmpty()) getComponent(subtitleKey, *params)
                 else Component.empty()
         player.showTitle(AdventureTitle.title(titleComponent, subtitleComponent, times))
+    }
+
+    /**
+     * Shows a title and subtitle with the given keys and optional parameters to all online players.
+     *
+     * @param titleKey The key associated with the title message in the message file.
+     * @param subtitleKey The key associated with the subtitle message in the message file, or an
+     * empty string for no subtitle.
+     * @param params Optional parameters to replace placeholders in the messages.
+     */
+    fun showToPlayers(titleKey: String, subtitleKey: String, vararg params: String) {
+        val titleComponent = getComponent(titleKey, *params)
+        val subtitleComponent =
+                if (subtitleKey.isNotEmpty()) getComponent(subtitleKey, *params)
+                else Component.empty()
+        for (player in Bukkit.getOnlinePlayers()) {
+            player.showTitle(AdventureTitle.title(titleComponent, subtitleComponent, times))
+        }
+    }
+
+    /**
+     * Retrieves associated with the given key, replaces placeholders and returns a string.
+     *
+     * @param key The key associated with the message in the message file.
+     * @param params Optional parameters to replace placeholders in the message.
+     */
+    private fun getString(key: String, vararg params: String): String {
+        var title = titles.getString(key)
+        if (title != null) {
+            for (i in params.indices) {
+                title = title?.replace("{" + (i + 1) + "}", params[i])
+            }
+        }
+        return title ?: key
+    }
+
+    /**
+     * Retrieves associated with the given key, replaces placeholders and returns a Component.
+     *
+     * @param key The key associated with the message in the message file.
+     * @param params Optional parameters to replace placeholders in the message.
+     */
+    private fun getComponent(key: String, vararg params: String): Component {
+        var message = getString(key, *params)
+        return miniMessage.deserialize(message)
     }
 }
